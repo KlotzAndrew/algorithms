08a066e760484a7b143f222de90df158d0bcaf30
Andrew Klotz <agc.klotz@gmail.com>
2016-07-12 21:02:54 -0400

    added quick sort

--- quick_sort.rb
+++ quick_sort.rb
@@ -1,4 +1,29 @@
 class QuickSort
-  def call(arr)
+  def call(arr, low_index, high_index)
+    if low_index < high_index
+      pivot = partition arr, low_index, high_index
+
+      call arr, low_index, pivot - 1
+      call arr, pivot + 1, high_index
+    end
+
+    arr
+  end
+
+  private
+
+  def partition(unsorted_array, low_index, high_index)
+    pivot_value = unsorted_array[high_index]
+    wall_index  = low_index
+
+    wall_index.upto(high_index - 1) do |current_index|
+      if unsorted_array[current_index] <= pivot_value
+        unsorted_array[current_index], unsorted_array[wall_index] = unsorted_array[wall_index], unsorted_array[current_index]
+        wall_index += 1
+      end
+    end
+    unsorted_array[wall_index], unsorted_array[high_index] = unsorted_array[high_index], unsorted_array[wall_index]
+
+    wall_index
   end
 end
---
+++ test/quick_sort_test.rb
@@ -0,0 +1,13 @@
+require "minitest/autorun"
+require_relative '../quick_sort.rb'
+
+class QuickSortTest < Minitest::Test
+  def test_call_method
+    assert_equal unsorted_array.sort,
+                 QuickSort.new.call(unsorted_array, 0, unsorted_array.length - 1)
+  end
+
+  def unsorted_array
+    [80, 18, 54, 72, 38, 91, 36, 21, 3, 28, 12, 4, 57, 31]
+  end
+end
